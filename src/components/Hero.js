b5b88d25833dab083de0967fa2dfd4f94096db3a
Roger Mai <cab022010@coderacademy.edu.au>
2021-01-22 03:23:22 +1000

    text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,26 +3,41 @@ import React from 'react'
 // import styled from 'styled-components'
 import styled, { css } from 'styled-components/macro';
 import { Button } from './Button';
-// import Video from '../images/video-3.mp4';
+import Image from '../images/Image-2.jpg';
 // import { IoMdArrowRoundForward } from 'react-icons/io';
 // import { IoArrowForward, IoArrowBack } from 'react-icons/io5';
 
 
-// const HeroContainer = styled.section`
-//     background: url(${Image}) center;
-//     height: 100vh;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
+const HeroContainer = styled.section`
+    background: url(${Image}) center;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-// `;
-
-const HeroContainer = styled.div`
-    
 `;
 
+
 const Container = styled.div`
-    
+    h1 {
+        display: flex;
+        color: #fff;
+        font-size: 50px;
+        justify-content: center;
+        align-items: center;
+
+    }
+
+    p {
+        display: flex;
+        color: #fff;
+        font-family: sans-serif; 
+        font-size: 25px;
+        padding: 10px;
+        margin: 0;
+
+
+    }
 `;
 
 
@@ -31,8 +46,6 @@ function Hero () {
     return (
         <HeroContainer>
             <Container>
-                <video src='/images/video-3.mp4'
-                autoPlay loop muted />
                 <h1>Larry's Lawncare</h1>
                 <p>With over 30 years’ experience in the Australian turf industry, we know our stuff when it comes to lawn care. Now, we’re opening the door to you.</p>
                 <p>Lawn Care Supplies is proudly backed by Larry's Lawncare, which has  serviced the turf industry since 1969. K&B Adams supplies premium turf maintenance products to industry professionals across Australia, from golf courses and bowls clubs to tennis courts and professional stadiums. </p>
